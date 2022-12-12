e8f4575e0ad13c6b4db8ba1912282a54a7915551
Jackpenn2019 <kibet.issa@gmail.com>
2022-12-12 22:18:46 +0000

    some fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,7 @@
 # Author - isaiahKC
 def islower(c):
     '''checks for lowercase character.'''
-    alphabet = []
-    start = ord('a')
-    for i in range(26):
-        alphabet.append(chr(start + i))
-    return(c in alphabet)
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
