770e444261ca5efef57b9665960d8311af4cdf9e
Jackpenn2019 <kibet.issa@gmail.com>
2022-12-20 05:44:21 +0000

    fixed

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,9 @@
 def safe_print_division(a, b):
     try:
         quotient = int(a) / int(b)
-    except Exception:
-        quotient = 'None'
+    except (TypeError, ZeroDivisionError):
+        quotient = None
     finally:
         print("Inside result: {}".format(quotient))
-        return quotient
+    return quotient
 
