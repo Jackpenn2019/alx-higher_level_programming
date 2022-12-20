5d7c0e4042b2bc968946c6771168b95321d10dd8
Jackpenn2019 <kibet.issa@gmail.com>
2022-12-20 04:45:34 +0000

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    # initializing the flag to 'True'
     flag = True
     
-    # try to convert value to integer
     try:
         value = int(value)
     
-    # catch the exception
     except ValueError:
         flag = False
 
-    # Else print the value
     else:
         print("{:d}".format(value))
+        print("")
     
-    # Finally always return the flag
     finally:
         return (flag)
