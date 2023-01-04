6f3104795e472f2a8a782d43bd20427eba8dea3e
Jackpenn2019 <kibet.issa@gmail.com>
2023-01-04 07:45:43 +0000

    fixed init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,23 +4,15 @@
 
 class Rectangle:
     """Define a Rectangle."""
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         """Initialize a Rectangle.
 
         Args:
             width(int): Width of the new Rectangle
             height(int): Height of the new Rectangle
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
